5ca6a623aad90b9e88732b31961836935c47af4c
sliver14 <silverchristopher12@gmail.com>
2026-07-28 21:12:14 +0100

    update

--- template-avatar-generator.php
+++ template-avatar-generator.php
@@ -9,6 +9,7 @@ Template Name: Avatar Generator
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Event Avatar Generator</title>
+  <link rel="icon" type="image/png" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/cropped-fav.png' ); ?>">
   
   <!-- Google Fonts: Poppins -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
